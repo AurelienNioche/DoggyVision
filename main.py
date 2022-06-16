5afbe5f10285df3ec5793e5886865548172c941e
AurelienNioche <nioche.aurelien@gmail.com>
2022-06-16 18:23:41 +0100

    update

---
+++ main.py
@@ -0,0 +1,50 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+def main():
+    model = NeuralNetwork()
+
+    training_data = ...
+
+    train_dataloader = DataLoader(training_data, batch_size=64)
+    # test_dataloader = DataLoader(test_data, batch_size=64)
+
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+
+if __name__ == '__main__':
+    main()
